6ab16efb46d8419a5b00b7c21304859472b374a3
TrongLuong <trongluong227@gmail.com>
2019-11-04 13:40:07 +0700

    update

--- app/src/main/java/com/example/kiemtrathuongky/DataSach.java
+++ app/src/main/java/com/example/kiemtrathuongky/DataSach.java
@@ -20,11 +20,12 @@ public class DataSach extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "sachlist";
 
     private static final String TABLE_Sach= "sach";
+    private static final String TABLE_TG = "tacgia";
 
     private static final String IDSACH = "idsach";
     private static final String TITLESACH = "titlesach";
 
-    private static final String TABLE_TG = "tacgia";
+
     private static final String AUTHORNAME = "authorname";
     private static final String IDAUTHOR = "idauthor";
 
@@ -42,15 +43,15 @@ public class DataSach extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         try {
             String sqlQuery1 = "CREATE TABLE "+TABLE_TG +" (" +
-                    IDAUTHOR + " integer primary key, "+
+                    IDAUTHOR + " integer primary key autoincrement, "+
                     AUTHORNAME +" TEXT)";
             sqLiteDatabase.execSQL(sqlQuery1);
 
             String sqlQuery2 = "CREATE TABLE "+TABLE_Sach +" (" +
                     IDSACH + " integer primary key, "+
                     TITLESACH + " TEXT ,"+
-                    IDAUTHOR + " integer " +
-                    "constraint IDAUTHOR  references TABLE_TG  (IDAUTHOR)"+
+                    IDAUTHOR + " integer not null " +
+                    "constraint IDAUTHOR  references TABLE_TG(IDAUTHOR)"+
                     "on delete cascade on update cascade)";
             sqLiteDatabase.execSQL(sqlQuery2);
             Toast.makeText(context, "Create successfylly", Toast.LENGTH_SHORT).show();
@@ -65,28 +66,57 @@ public class DataSach extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_Sach);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_TG);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_Sach);
+
         onCreate(sqLiteDatabase);
         Toast.makeText(context, "Drop successfylly", Toast.LENGTH_SHORT).show();
     }
     //them author
-    public int addTG(Author author){
-        int rs = 0;
-        try {
-            SQLiteDatabase  db = this.getWritableDatabase();
-            ContentValues values = new ContentValues();
-            values.put(IDAUTHOR, author.getIda());
-            values.put(AUTHORNAME, author.getNamea());
-             rs = (int) db.insert(TABLE_TG,null, values);
-            db.close();
+    public boolean addTG(Author author){
+        if (author == null)
+            return false;
 
-        }catch (SQLiteConstraintException e){
-            e.printStackTrace();
-        }
-        return  rs;
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(IDAUTHOR, author.getIda());
+        cv.put(AUTHORNAME, author.getNamea());
+
+
+        db.insert(TABLE_TG, null, cv);
+        return true;
     }
-    //lay toan bo author
+    //update author
+    public int updateAuthor(Author author) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(AUTHORNAME, author.getNamea());
+        return db.update(TABLE_TG, values, IDAUTHOR + "=?", new String[]{String.valueOf(author.getIda())});
+
+    }
+    //delete
+    public void deleteteAuthor(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_TG, "IDAUTHOR =" + id, null);
+        db.close();
+    }
+    //search author by id
+    public Author getAuthorID(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor;
+        Author author ;
+
+        cursor = db.query(TABLE_TG, new String[]{IDAUTHOR,AUTHORNAME},IDAUTHOR + " =? ",
+                new String[]{String.valueOf(id)},null,null,null,null);
+        if(cursor!=null)
+            cursor.moveToFirst();
+        author  = new Author( cursor.getInt(0),cursor.getString(1));
+        cursor.close();
+        db.close();
+
+        return author;
+    }
+    //LAY TATA CA
     public ArrayList<Author> getAllAuthor(){
         ArrayList<Author> authorArrayList = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
@@ -102,31 +132,44 @@ public class DataSach extends SQLiteOpenHelper {
         db.close();
         return authorArrayList;
     }
-    //lay toan bo author
-    public ArrayList<Sach> getAllSach(){
-        ArrayList<Sach> sachArrayList = new ArrayList<>();
-        SQLiteDatabase db = this.getReadableDatabase();
-        String qr = "SELECT  * FROM " + TABLE_Sach;
-        Cursor cursor = db.rawQuery(qr,null);
-        if(cursor!=null)
-            cursor.moveToFirst();
-        while (cursor.isAfterLast()==false){
-            sachArrayList.add(new Sach(cursor.getInt(0),cursor.getString(1), cursor.getInt(2)));
-            cursor.moveToNext();
-        }
-        cursor.close();
-        db.close();
-        return sachArrayList;
+
+
+    // ------------------------------- Quản lý BOOKS -------------------------------//
+
+
+
+
+    //lay toan bo sach
+    public boolean insertBook (Sach sach) {
+        if (sach == null)
+            return false;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(IDSACH, sach.getId());
+        cv.put(TITLESACH, sach.getTitle());
+        cv.put(IDAUTHOR, sach.getIda());
+
+        db.insert(TABLE_Sach, null, cv);
+        return true;
     }
-    //lay sach
+    public int updateSach(Sach sach) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(TITLESACH, sach.getTitle());
+        return db.update(TABLE_Sach, values, IDSACH + "=?", new String[]{String.valueOf(sach.getId())});
+
+    }
+    //lay sach theo id
     public Sach getsachID(int id){
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_TG, new String[]{IDAUTHOR,AUTHORNAME},IDAUTHOR + " =? ",
+        Cursor cursor = db.query(TABLE_TG, new String[]{IDSACH,TITLESACH,IDAUTHOR},IDSACH + " =? ",
                 new String[]{String.valueOf(id)},null,null,null,null);
 
         if(cursor!=null)
             cursor.moveToFirst();
-        Author author = new Author( cursor.getInt(0),   cursor.getString(1));
+      //  Sach sach = new Sach( cursor.getInt(0),   cursor.getString(1), cursor.getInt(2));
         Sach sach = new Sach(cursor.getInt(0), cursor.getString(1), cursor.getInt(2));
         cursor.close();
         db.close();
@@ -149,39 +192,29 @@ public class DataSach extends SQLiteOpenHelper {
         }
         return  rs;
     }
-
-//update author
-    public int updateAuthor(Author author) {
+    //xoa sacsh
+    public void deleteSach(int id) {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-            values.put(AUTHORNAME, author.getNamea());
-           return db.update(TABLE_TG, values, IDAUTHOR + "=?", new String[]{String.valueOf(author.getIda())});
-
-
-
-    }
-//delete
-    public void deleteteAuthor(int id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_TG, "IDAUTHOR =" + id, null);
+        db.delete(TABLE_TG, "IDSACH =" + id, null);
         db.close();
     }
-    //search author by id
-    public Author getAuthorID(int id){
+    public ArrayList<Sach> getAllSach(){
+        ArrayList<Sach> sachArrayList = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor;
-        Author author ;
-
-            cursor = db.query(TABLE_TG, new String[]{IDAUTHOR,AUTHORNAME},IDAUTHOR + " =? ",
-                    new String[]{String.valueOf(id)},null,null,null,null);
-            if(cursor!=null)
-                cursor.moveToFirst();
-            author  = new Author( cursor.getInt(0),cursor.getString(1));
-            cursor.close();
-            db.close();
-
-
-
-        return author;
+        String qr = "SELECT  * FROM " + TABLE_Sach;
+        Cursor cursor = db.rawQuery(qr,null);
+        if(cursor!=null)
+            cursor.moveToFirst();
+        while (cursor.isAfterLast()==false){
+            sachArrayList.add(new Sach(cursor.getInt(0),cursor.getString(1),cursor.getInt(2)));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        db.close();
+        return sachArrayList;
     }
+
+
+
+
 }
--- app/src/main/java/com/example/kiemtrathuongky/MainActivity.java
+++ app/src/main/java/com/example/kiemtrathuongky/MainActivity.java
@@ -4,15 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 
+import android.widget.SpinnerAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -21,6 +25,23 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     Dialog dialog;
     DataSach dataSach = new DataSach(this);
+    EditText id, authorname;
+    GridView gridView;
+    ArrayList<Author> authorArrayList;
+    Button btnselect, btnupdate, btnsave, btnExit, btndelete;
+//sach
+    EditText ids, title;
+    Button btnselectS, btnupdateS, btnsaveS, btnExitS, btndeleteS;
+    Spinner spinnerAthor;
+    GridView gridViewS;
+
+    ArrayList<Sach> sachArrayList;
+    ArrayList<String> sach_authors;
+    ArrayList<String> array_BookToString;
+    ArrayAdapter<String> adater_Spiner;
+    ArrayAdapter<String> adapter_books;
+
+
 
 
     @Override
@@ -28,71 +49,97 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
-
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.ttsach: xemChTiet();
-                break;}
+            case R.id.ttsach: xemChTietSach();
+                break;
+            case R.id.tttg:
+                xemChTiet();
+                break;
+        }
+
         return super.onOptionsItemSelected(item);
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+    }
+    public void initAuthor(Dialog dialog) {
+
+        id = dialog.findViewById(R.id.edtIDA);
+        authorname = dialog.findViewById(R.id.edtNameA);
+        gridView = dialog.findViewById(R.id.gv);
+
+        btnselect = dialog.findViewById(R.id.btnSelect);
+        btnupdate = dialog.findViewById(R.id.btnUpdate);
+        btnsave = dialog.findViewById(R.id.btnSave);
+        btndelete = dialog.findViewById(R.id.btnDelete);
+        btnExit = dialog.findViewById(R.id.btnexit);
+
+    }
+    //SACH
+    public void initSach(Dialog dialog) {
+
+        ids = dialog.findViewById(R.id.edtIDS);
+        title = dialog.findViewById(R.id.edtTitle);
+        gridViewS = dialog.findViewById(R.id.gvS);
+
+        btnselectS = dialog.findViewById(R.id.btnSelectS);
+        btnupdateS = dialog.findViewById(R.id.btnUpdateS);
+        btnsaveS = dialog.findViewById(R.id.btnSaveS);
+        btndeleteS = dialog.findViewById(R.id.btnDeleteS);
+        btnExitS = dialog.findViewById(R.id.btnexitS);
+        spinnerAthor = dialog.findViewById(R.id.spi_bAuthorId);
 
     }
 
+    //hien thi author len gird Author
+    public void displayGird(){
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Author> authorArrayList;
+        authorArrayList = dataSach.getAllAuthor();
 
+        for (Author a : authorArrayList) {
+            list.add(a.getIda() + "");
+            list.add(a.getNamea());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, list);
+        gridView.setAdapter(adapter);
+
+    }
+    //hien thi sach
+    public void displayGirdSach(){
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Sach> sachArrayList;
+        sachArrayList = dataSach.getAllSach();
+
+        for (Sach a : sachArrayList) {
+            list.add(a.getId() + "");
+            list.add(a.getTitle());
+            list.add(a.getIda()+"");
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, list);
+        gridViewS.setAdapter(adapter);
+    }
+
+    //Author
     public void xemChTiet() {
-
         dialog = new Dialog(this);
         dialog.setContentView(R.layout.dialog);
         dialog.show();
         //khóa dialog, khi click ngoài k bị mất
         dialog.setCancelable(false);
-        final EditText id = dialog.findViewById(R.id.edtIDA);
-        final EditText title = dialog.findViewById(R.id.edtTitle);
-        final EditText authorname = dialog.findViewById(R.id.edtNameA);
+        initAuthor(dialog);
+        authorArrayList = new ArrayList<>();
 
-        final  GridView gridView = dialog.findViewById(R.id.gv);
-
-
-        final Button btnselect = dialog.findViewById(R.id.btnSelect);
-        final Button btnupdate = dialog.findViewById(R.id.btnUpdate);
-        final Button btnsave = dialog.findViewById(R.id.btnSave);
-        final Button btndelete = dialog.findViewById(R.id.btnDelete);
-        final Button btnExit = dialog.findViewById(R.id.btnexit);
-
-
-        dataSach.addTG(new Author(1, "Navathe"));
-        dataSach.addTG(new Author(2, "Navathe"));
-        dataSach.addTG(new Author(3, "Navathe"));
-        dataSach.addTG(new Author(4, "Navathe"));
-
-
-        dataSach.addSach(new Sach(1, "xxx",1));
-        dataSach.addSach(new Sach(2, "xxx",2));
-        dataSach.addSach(new Sach(3, "xxx",3));
-        dataSach.addSach(new Sach(4, "xxx",4));
-
-
-
-
-        ArrayList<String> list = new ArrayList<>();
-        ArrayList<Author> authorArrayList;
-        authorArrayList = dataSach.getAllAuthor();
-        for(Author a : authorArrayList){
-            list.add(a.getIda() + "");
-            list.add(a.getNamea());
-           list.add(a.getIda()+"");
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, list);
-        gridView.setAdapter(adapter);
-
+        dataSach.addTG(new Author(1, "Trong"));
+        dataSach.addTG(new Author(2, "Truc"));
+        dataSach.addTG(new Author(3, "Trinh"));
+        dataSach.addTG(new Author(4, "Trang"));
 
         btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,79 +150,172 @@ public class MainActivity extends AppCompatActivity {
         btnsave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Author author = new Author(Integer.parseInt(id.getText().toString()),authorname.getText().toString());
-               dataSach.addTG(author);
-               Toast.makeText(MainActivity.this, "Them thanh cong!", Toast.LENGTH_SHORT).show();
-
-                ArrayList<String> list = new ArrayList<>();
-                ArrayList<Author> authorArrayList;
-                authorArrayList = dataSach.getAllAuthor();
-                for(Author a : authorArrayList){
-                    list.add(a.getIda() + "");
-                    list.add(a.getNamea());
+                Author author = new Author(Integer.parseInt(id.getText().toString()), authorname.getText().toString());
+                if (authorArrayList.contains(author))
+                    Toast.makeText(MainActivity.this, "Trùng mã", Toast.LENGTH_SHORT).show();
+                else if (dataSach.addTG(author)) {
+                    authorArrayList.add(author);
+                    Toast.makeText(MainActivity.this, "Thêm tác giả thành công", Toast.LENGTH_SHORT).show();
                 }
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, list);
-                gridView.setAdapter(adapter);
-
-
             }
         });
         btndelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
                 dataSach.deleteteAuthor(Integer.parseInt(id.getText().toString()));
                 Toast.makeText(MainActivity.this, "Xoa thanh cong!", Toast.LENGTH_SHORT).show();
-
-                ArrayList<String> list = new ArrayList<>();
-                ArrayList<Author> authorArrayList;
-                authorArrayList = dataSach.getAllAuthor();
-                for(Author a : authorArrayList){
-                    list.add(a.getIda() + "");
-                    list.add(a.getNamea());
-                }
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, list);
-                gridView.setAdapter(adapter);
+               displayGird();
             }
         });
         btnupdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Author at = dataSach.getAuthorID(Integer.parseInt(id.getText().toString()));
 
-                Author at =  dataSach.getAuthorID(Integer.parseInt(id.getText().toString()));
-                //at.setId(Integer.parseInt(id.getText().toString()));
                 at.setNamea(authorname.getText().toString());
                 dataSach.updateAuthor(at);
-
                 Toast.makeText(MainActivity.this, "Cap nhat thanh cong!", Toast.LENGTH_SHORT).show();
-
-                ///load lai girdview
-                ArrayList<String> list = new ArrayList<>();
-                ArrayList<Author> authorArrayList;
-                authorArrayList = dataSach.getAllAuthor();
-                for(Author a : authorArrayList){
-                    list.add(a.getIda() + "");
-                    list.add(a.getNamea());
-                }
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, list);
-                gridView.setAdapter(adapter);
+                displayGird();
 
             }
         });
         btnselect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                displayGird();
+            }
+        });
+
+    }
+    //sach
+    public void setAdapter_books() {
+        if (adapter_books == null) {
+            adapter_books = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, array_BookToString);
+            gridViewS.setAdapter(adapter_books);
+        } else
+            adapter_books.notifyDataSetChanged();
+    }
+    public void xemChTietSach() {
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_sach);
+        dialog.show();
+        //khóa dialog, khi click ngoài k bị mất
+        dialog.setCancelable(false);
+        initSach(dialog);
+
+
+        // truyền spiner
+//        authorArrayList = dataSach.getAllAuthor();
+//        sach_authors = new ArrayList<>();
+//        for (Author a : authorArrayList)
+//            sach_authors.add(a.getIda() + " - " + a.getNamea());
+//
+//
+//        adater_Spiner = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, sach_authors);
+//        adater_Spiner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinnerAthor.setAdapter(adater_Spiner);
+//        spinnerAthor.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Toast.makeText(MainActivity.this, spinnerAthor.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("3");
+        list.add("5");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,list);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spinnerAthor.setAdapter(adapter);
+        spinnerAthor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent();
+//                Bundle b = new Bundle();
+//                b.putString("s", spinnerAthor.getSelectedItem().toString());
+//                intent.putExtra("data",b);
+//                startActivity(intent);
+               Toast.makeText(MainActivity.this, spinnerAthor.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        // truyền gridview
+        array_BookToString = new ArrayList<>();
+        sachArrayList = dataSach.getAllSach();
+
+        // chuyển thành string để đưa vào array_BookToString
+        for (Sach b : sachArrayList) {
+            array_BookToString.add(b.getId() + "");
+            array_BookToString.add(b.getTitle());
+            array_BookToString.add(b.getIda() + "");
+        }
 
 
 
+        btnExitS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        //chua sua
+        btnsaveS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = getIntent();
+//                Bundle bundle = intent.getBundleExtra("data");
+//                String x =bundle.getString("s");
+
+                Sach sach = new Sach(Integer.parseInt(ids.getText().toString()), title.getText().toString(),1);
+                if (sachArrayList.contains(sach))
+                    Toast.makeText(MainActivity.this, "Trùng mã", Toast.LENGTH_SHORT).show();
+                else if (dataSach.insertBook(sach)) {
+                    sachArrayList.add(sach);
+                    Toast.makeText(MainActivity.this, "Thêm sach thành công", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btndeleteS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dataSach.deleteteAuthor(Integer.parseInt(id.getText().toString()));
+                Toast.makeText(MainActivity.this, "Xoa thanh cong!", Toast.LENGTH_SHORT).show();
+                displayGird();
+            }
+        });
+        btnupdateS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Author at = dataSach.getAuthorID(Integer.parseInt(id.getText().toString()));
+
+                at.setNamea(authorname.getText().toString());
+                dataSach.updateAuthor(at);
+                Toast.makeText(MainActivity.this, "Cap nhat thanh cong!", Toast.LENGTH_SHORT).show();
+                displayGird();
+
+            }
+        });
+        btnselectS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayGirdSach();
 
 
             }
         });
 
     }
-
 
 
 }
